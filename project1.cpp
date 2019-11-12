f9fe08de5425686a1bcb497b943dff489948ae3b
chowais181 <56623836+chowais181@users.noreply.github.com>
2019-11-12 17:14:18 +0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 int main()
 {
-cout<<"Hello in the World of Git!"<<endl;
   for(int i=0;i<10;i++)
   {
 cout<<"How are you!"<<endl;
   }
-system("pause")
+system("paause>0")
 }
