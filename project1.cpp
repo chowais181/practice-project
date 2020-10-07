bca1dc95692e35e36d65b84f0d56afcea9f65d09
chowais181 <56623836+chowais181@users.noreply.github.com>
2020-10-07 11:04:02 -0700

    Revert "Update project1.cpp"

--- project1.cpp
+++ project1.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 int main()
 {
-  char arr = {'z','e','e','s','h','a','n'};
 cout<<"Hello in the World of Git!"<<endl;
-cout<<"Here is ";
-for(int i=0;i<7;i++)
-  cout<<arr[i];
-cout<<endl;
+cout<<"How are you!"<<endl;
 system("pause")
 }
